5c754b0bc8d41b0cbb70db4a07fd5deed73e2a7b
Erik OLeary <erik.m.oleary@gmail.com>
2020-06-19 14:10:53 -0500

    Stubbed tests

---
+++ Tests/LockTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Wivuu.GlobalCache;
+using Xunit;
+
+namespace Tests
+{
+    public class LockTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ Tests/SerializationTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Wivuu.GlobalCache;
+using Xunit;
+
+namespace Tests
+{
+    public class SerializationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
