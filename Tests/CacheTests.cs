335f2d90577e4becddf897fa27669d135bf468e8
erikoleary <erik.m.oleary@gmail.com>
2020-06-19 16:49:21 -0500

    Set langversion to latest

---
+++ Tests/CacheTests.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Wivuu.GlobalCache;
+using Xunit;
+
+namespace Tests
+{
+    public class CacheTests
+    {
+        public CacheTests()
+        {
+            var collection = new ServiceCollection();
+
+            Services = collection.BuildServiceProvider();
+        }
+
+        public ServiceProvider Services { get; }
+
+        [Fact]
+        public async Task TestGeneralCaching()
+        {
+            using var scope = Services.CreateScope();
+
+            var cache = scope.ServiceProvider.GetRequiredService<IGlobalCache>();
+
+            var item = await cache.GetOrCreateAsync(new CacheIdentity("Test"), async e =>
+            {
+                return 0;
+            });
+        }
+    }
+}
