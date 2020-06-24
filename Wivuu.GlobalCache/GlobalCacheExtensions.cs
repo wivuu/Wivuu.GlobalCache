42740cb19f936885d9f433249adf31c25fa480f8
Erik OLeary <erik.m.oleary@gmail.com>
2020-06-24 12:57:09 -0500

    Declare nullable

--- Wivuu.GlobalCache/GlobalCacheExtensions.cs
+++ Wivuu.GlobalCache/GlobalCacheExtensions.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class GlobalCacheExtensions
     {
-        public static IServiceCollection AddWivuuGlobalCache(this IServiceCollection collection, Action<GlobalCacheSettings> configure = default)
+        public static IServiceCollection AddWivuuGlobalCache(this IServiceCollection collection, Action<GlobalCacheSettings>? configure = default)
         {
             if (configure != null)
                 collection.Configure<GlobalCacheSettings>(configure);
