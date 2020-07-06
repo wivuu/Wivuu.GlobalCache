e0d3739fd8c03819a3844edd1a9aad879f579ab0
Erik OLeary <erik.m.oleary@gmail.com>
2020-07-06 16:44:59 -0500

    Default options

--- Wivuu.GlobalCache/GlobalCacheExtensions.cs
+++ Wivuu.GlobalCache/GlobalCacheExtensions.cs
@@ -13,10 +13,17 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddWivuuGlobalCache(this IServiceCollection collection,
                                                              Action<GlobalCacheSettings>? configure = default)
         {
-            if (configure != null)
-                collection.Configure<GlobalCacheSettings>(configure);
-            else
-                collection.Configure<GlobalCacheSettings>(settings => {});
+            collection.Configure<GlobalCacheSettings>(options =>
+            {
+                if (configure != null)
+                    configure(options);
+
+                if (options.StorageProvider == null)
+                    options.StorageProvider = new FileStorageProvider();
+
+                if (options.SerializationProvider == null)
+                    options.SerializationProvider = new JsonSerializationProvider();
+            });
 
             collection.AddSingleton<IGlobalCache, DefaultGlobalCache>();
 
